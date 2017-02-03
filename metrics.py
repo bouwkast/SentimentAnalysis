625a535213a1dc0f3b791666e71d8e08215f6861
Steven Bouwkamp <lordstevex@gmail.com>
2017-02-03 09:10:46 -0500

    added some files - about to do some analysis on data

---
+++ metrics.py
@@ -0,0 +1,10 @@
+"""
+This file will scrape some metrics from our data so that we can build our
+Tensors more intelligently - looking for word counts, review length, useless words, etc.
+Filename: metrics.py
+Author: Steven
+Date Last Modified: 2/3/2017
+Email: bouwkast@mail.gvsu.edu
+"""
+
+import matplotlib as plt
---
+++ tensor_flow_sa.py
@@ -0,0 +1,10 @@
+"""
+
+Filename: tensor_flow_sa.py
+Author: Steven
+Date Last Modified: 2/2/2017
+Email: bouwkast@mail.gvsu.edu
+
+
+Soon this will be a Sentiment Analysis using TensorFlow
+"""
