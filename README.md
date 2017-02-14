9e86a52de6695c0389f0d02e5ba5780a4fb0d348
Steven Bouwkamp <bouwkast@mail.gvsu.edu>
2017-02-13 21:42:35 -0500

    Initial tests with parameter optimization and different classifiers.

---
+++ README.md
@@ -0,0 +1,10 @@
+## Sentiment Analysis on Movie Reviews
+
+Steven Bouwkamp
+Andy Vuong
+Matt Schuch
+
+The goal of this project is to train a model with given movie reviews with known values of either being
+a positive or negative review.
+The data provided hasn't been cleaned (HTML tags, foreign characters, fluff words) are present and must be
+removed to achieve a more accurate model.
--- basic_sentiment_pipeline.py
+++ basic_sentiment_pipeline.py
@@ -7,17 +7,23 @@
 
 import pandas as pd
 import pickle
+
+from sklearn.grid_search import GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import GaussianNB
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+
+from sklearn.linear_model import SGDClassifier as SGD
 
 # Hint: These are not actually used in the current 
 # pipeline, but would be used in an alternative 
 # tokenizer such as PorterStemming.
 # import nltk
 # nltk.download('stopwords')
-# from nltk.corpus import stopwords
+
 # stop = stopwords.words('english')
 
 """
@@ -26,11 +32,13 @@ from sklearn.naive_bayes import GaussianNB
     Hint: Perhaps implement others such as PorterStemming
     Hint: Is this even used?  Where would you place it?
 """
+
+porter_stem = PorterStemmer()
 def tokenizer(text):
-    return text.split()
+    return [porter_stem.stem(word) for word in text.split()]
 
 # Read in the dataset and store in a pandas dataframe
-df = pd.read_csv('./training_movie_data_cleaned.csv', encoding='ascii')
+df = pd.read_csv('./training_movie_data_cleaned.csv')
 
 # Split your data into training and test sets.
 # Allows you to train the model, and then perform
@@ -49,9 +57,16 @@ y_test = df.loc[training_size:, 'sentiment'].values
 # Hint: Perhaps there are different preprocessors to
 # test?
 
-tfidf = TfidfVectorizer(strip_accents=None,
+tfidf = TfidfVectorizer(strip_accents='unicode',
+                        analyzer='word',
+                        stop_words='english',
                         lowercase=False,
-                        preprocessor=None)
+                        preprocessor=None,
+                        tokenizer=tokenizer)
+
+# tfidf = TfidfVectorizer(strip_accents=None,
+#                         lowercase=False,
+#                         preprocessor=None)
 
 # Hint: There are methods to perform parameter sweeps to find the
 # best combination of parameters.  Look towards GridSearchCV in 
@@ -62,19 +77,49 @@ tfidf = TfidfVectorizer(strip_accents=None,
 # Look to documentation on Regression or similar methods for hints.
 # Possibly investigate alternative classifiers for text/sentiment.
 
+
+# param_grid = [{'vect__ngram_range': [(1, 1)],
+#                'vect__stop_words': ['english', None],
+#                'vect__tokenizer': [tokenizer, None],
+#                'clf__penalty': ['l1', 'l2'],
+#                'clf__C': [1.0, 10.0, 100.0]},
+#               {'vect__ngram_range': [(1, 1)],
+#                'vect__stop_words': ['english', None],
+#                'vect__tokenizer': [tokenizer, None],
+#                'vect__use_idf':[False, True],
+#                'vect__norm':[None],
+#                'clf__penalty': ['l1', 'l2'],
+#                'clf__C': [1.0, 10.0, 100.0]},
+#               ]
+
+# param_grid_2 = [{'clf__penalty': ['l1', 'l2']}]
+
 #  http://scikit-learn.org/stable/auto_examples/linear_model/plot_logistic_l1_l2_sparsity.html
 #  Above link for info on the value C in the classifier (clf)
 lr_tfidf = Pipeline([('vect', tfidf),
-                     ('clf', LogisticRegression(C=1.00,fit_intercept=False,penalty='l2',random_state=0))])
+                     ('clf', SGD())])
+
+# gs_lr_tfidf = GridSearchCV(lr_tfidf, param_grid_2,
+#                            scoring='accuracy',
+#                            cv=5,
+#                            verbose=1)
 
 
-param_grid = []
+# gs_lr_tfidf.fit(X_train, y_train)
+#
+# print('BEST PARAM: ')
+# print(gs_lr_tfidf.best_params_)
+#
+# print('CV Accuracy: %.3f' % gs_lr_tfidf.best_score_)
+#
+# clf = gs_lr_tfidf.best_estimator_
+# print('Test Accuracy: %.3f' % clf.score(X_test, y_test))
 
-# Train the pipline using the training set.
+# # Train the pipline using the training set.
 lr_tfidf.fit(X_train, y_train)
-
-# Print the Test Accuracy
+#
+# # Print the Test Accuracy
 print('Test Accuracy: %.3f' % lr_tfidf.score(X_test, y_test))
-
-# Save the classifier for use later.
+#
+# # Save the classifier for use later.
 pickle.dump(lr_tfidf, open("saved_model.sav", 'wb'))
