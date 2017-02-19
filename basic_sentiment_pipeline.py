e18514c443b22bd58a1a5b35451a355a226c479c
Steven Bouwkamp <bouwkast@mail.gvsu.edu>
2017-02-19 02:06:17 -0500

    Get around 91% or so - Cross Validation output below
    [ 0.91557432  0.92135081  0.91711111  0.91288889  0.91222222  0.90533333
      0.91066667  0.91111111  0.90975772  0.91398088]

--- basic_sentiment_pipeline.py
+++ basic_sentiment_pipeline.py
@@ -1,49 +1,47 @@
 """
-    Train a logistic regression model for document classification.
+    Train a stochastic gradient descent model for movie review classification.
 
-    Search this file for the keyword "Hint" for possible areas of
-    improvement.  There are of course others.
 """
+
+# TODO - finish commenting this file
+# TODO - clean up the other helper files (need to turn in most likely)
+# TODO - maybe run some final GridSearchCVs - don't have to if don't want to
+# TODO - remove print statements of the cross validation scores
+# TODO - maybe make a logger? might help for overall completeness
+# TODO - don't remove gridsearchcv - keep it there as a reference for how we calculated parameters
+# TODO - don't remove porterstemmer or wordpunct - good ideas just they got trumped by bigrams
+# TODO - WRITE UP
+# TODO - finish the README
+
+
+# TODO - ON FEB 22nd turn repo to public (unless people haven't turned in)
+
+
+
 
 import pandas as pd
 import pickle
 import numpy as np
 import re
 
-from sklearn.grid_search import GridSearchCV
-from sklearn.linear_model import LassoCV
 from sklearn.pipeline import Pipeline
-from sklearn.linear_model import LogisticRegression, LogisticRegressionCV, SGDRegressor
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-from nltk.tokenize import word_tokenize, wordpunct_tokenize
-from sklearn.naive_bayes import GaussianNB
+from nltk.tokenize import wordpunct_tokenize
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
-
+from sklearn.model_selection import StratifiedKFold, cross_val_score
 from sklearn.linear_model import SGDClassifier as SGD
 
-# Hint: These are not actually used in the current 
-# pipeline, but would be used in an alternative 
-# tokenizer such as PorterStemming.
-# import nltk
-# nltk.download('stopwords')
-
-# stop = stopwords.words('english')
-
-"""
-    This is a very basic tokenization strategy.  
-    
-    Hint: Perhaps implement others such as PorterStemming
-    Hint: Is this even used?  Where would you place it?
-
-    Yeah PorterStemming decreases accuracy by .2% for some stupid reason.
-"""
-
+#  porter stemming decreases accuracy by .2%
 porter_stem = PorterStemmer()
 
-stop_words = set(stopwords.words('english')).union({'.', 'I', 'i', ',', '\'', 'it', '*', '?', '/', '-', '&', '<', '>', '\"'})
+#  these are the default stopwords along with some added punctuation - no longer used.
+stop_words = set(stopwords.words('english')).union(
+    {'.', 'I', 'i', ',', '\'', 'it', '*', '?', '/', '-', '&', '<', '>', '\"'})
 
+# The commented out lines for tokenizer and preprocessor are the more 'advanced'
+# preprocessing steps - but they lower accuracy, so the basic ones are in there instead.
 
 def tokenizer(text):
     return text.split()
@@ -58,93 +56,52 @@ def preprocessor(text):
     return new_text
     # return output
 
+
 # this is to protect for running multiple jobs for gridsearchcv
 if __name__ == '__main__':
     # Read in the dataset and store in a pandas dataframe
     df = pd.read_csv('./training_movie_data_cleaned.csv')
-    np.random.seed(0)  # seed for reproducibility
-    df = df.reindex(np.random.permutation(df.index))
+    magic_number = 5233  # this is a magic number, that is all
+    np.random.seed(magic_number)  # seed for reproducibility
+    df = df.reindex(np.random.permutation(df.index))  # 'shuffle' the dataset
 
-    # Split your data into training and test sets.
-    # Allows you to train the model, and then perform
-    # validation to get a sense of performance.
-    #
-    # Hint: This might be an area to change the size
-    # of your training and test sets for improved
-    # predictive performance.
-    training_size = 40000
+    training_size = 20000  # training size seems to be best at 20K
+    # TODO - with KFold we won't need this - train on all data soon
 
+    skf = StratifiedKFold(n_splits=10)  # 10 is a very common/standard split ratio
+
+    X = df['review'].values.astype('U')  # reviews
+    y = df['sentiment'].values  # sentiment
+
+    skf.get_n_splits(X, y)  # make n_splits for cross validation
+
+    #  TODO - these aren't needed
     X_train = df.loc[:training_size, 'review'].values.astype('U')
     y_train = df.loc[:training_size, 'sentiment'].values
     X_test = df.loc[training_size:, 'review'].values.astype('U')
     y_test = df.loc[training_size:, 'sentiment'].values
 
-    # Perform feature extraction on the text.
-    # Hint: Perhaps there are different preprocessors to
-    # test?
-
+    #  After a lot of GridSearching these seem to be the best
     tfidf = TfidfVectorizer(strip_accents='unicode',
                             analyzer='word',
                             stop_words=None,
                             lowercase=False,
                             preprocessor=preprocessor,
                             tokenizer=tokenizer,
-                            max_df=.9,
-
-                            max_features=100000,
+                            max_df=.05,
+                            min_df=2,
+                            max_features=80000,
                             sublinear_tf=True,
                             ngram_range=(1, 2))
 
-    # exit(0)
-
-    # Hint: There are methods to perform parameter sweeps to find the
-    # best combination of parameters.  Look towards GridSearchCV in
-    # sklearn or other model selection strategies.
-
-    # Create a pipeline to vectorize the data and then perform regression.
-    # Hint: Are there other options to add to this process?
-    # Look to documentation on Regression or similar methods for hints.
-    # Possibly investigate alternative classifiers for text/sentiment.
-
-    # TODO to run the grid search cv - uncomment the following
-    #
-    # param_grid = [{'vect__ngram_range': [(1, 1)],
-    #                'vect__stop_words': ['english', None],
-    #                'vect__tokenizer': [tokenizer, None],
-    #                'clf__penalty': ['l1', 'l2'],
-    #                'clf__C': [1.0, 10.0, 100.0]},
-    #               {'vect__ngram_range': [(1, 1)],
-    #                'vect__stop_words': ['english', None],
-    #                'vect__tokenizer': [tokenizer, None],
-    #                'vect__use_idf':[False, True],
-    #                'vect__norm':[None],
-    #                'clf__penalty': ['l1', 'l2'],
-    #                'clf__C': [1.0, 10.0, 100.0]},
-    #               ]
     # TODO - only choose one of the parameter grids at a time
-    param_grid_2 = [{'clf__penalty': ['l2', 'l1', 'elasticnet'],
-                     'clf__l1_ratio': [0.05, 0.15, 0.35, 0.5],
-                     'clf__fit_intercept': [True, False],
-                     'clf__shuffle': [True, False],
-                     'clf__learning_rate': ['optimal']
-                     }]
+    param_grid_2 = [{'vect__max_df': [.75, .5, .1, .075]}]
 
-    #  http://scikit-learn.org/stable/auto_examples/linear_model/plot_logistic_l1_l2_sparsity.html
-    #  Above link for info on the value C in the classifier (clf)
-
-    # LIST OF BEST PARAMS FOR SGC:
-    #   loss = 'modified_huber' SCORE = 0.906
-    #   penalty = 'l2' SCORE = 0.906; this is the default value
-    #   alpha = 0.00015 SCORE = 0.906; default is 0.0001 gets a 0.906 too
-    #   n_iter = 10 SCORE = 0.907; default is 5 - this is the number of epochs
-    #   SGD max score is 0.907
     # TODO - don't comment this out - unless you want to change the classifier
     lr_tfidf = Pipeline([('vect', tfidf),
-                         ('clf', SGD(loss='modified_huber', alpha=0.00015, n_iter=20, random_state=5, l1_ratio=0.05, penalty='l2', shuffle=False, learning_rate='optimal'))])
-
-    # TODO - was using this one as a test classifier (ie changing the classifier)
-    # lr_tfidf = Pipeline([('vect', tfidf),
-    #                      ('clf', LassoCV(max_iter=20))])
+                         ('clf', SGD(loss='modified_huber', alpha=0.00015, n_iter=np.ceil(10 ** 6 / training_size),
+                                     random_state=5, l1_ratio=0.05, penalty='l2', shuffle=False,
+                                     learning_rate='optimal'))])
 
     # # TODO - uncomment this to run the grid search with cross validation
     # gs_lr_tfidf = GridSearchCV(lr_tfidf, param_grid_2,
@@ -170,14 +127,32 @@ if __name__ == '__main__':
 
     #  #  TODO - comment out the following to run gridsearchcv
     # # Train the pipline using the training set.
-    lr_tfidf.fit(X_train, y_train)
+
+    # Train n_fold different models on each kfold and see how they perform
+    scores = cross_val_score(lr_tfidf, X, y, cv=skf)
+    print(scores)
+    #  After we cross validate to ensure that the model can predict unseen data - train on everything
+    lr_tfidf.fit(X, y)
+
+    # print('Test Accuracy: %.3f' % lr_tfidf.score(X_test, y_test))
+
+    # total = 0
+    # for train_index, test_index in skf.split(X, y):
+    #     print("TRAIN:", train_index, "TEST:", test_index)
+    #
+    #     X_train, X_test = X[train_index], X[test_index]
+    #     y_train, y_test = y[train_index], y[test_index]
+    #
+    #     lr_tfidf.fit(X_train, y_train)
+    #     print('Test Accuracy: %.3f' % lr_tfidf.score(X_test, y_test))
+    #     total += lr_tfidf.score(X_test, y_test)
+    # print(total / 10)
+    # print('end')
+
     #
     # # Print the Test Accuracy
-    print('Test Accuracy: %.3f' % lr_tfidf.score(X_test, y_test))
-    #
-    # # Save the classifier for use later.
+
+
+    # Save the classifier for use later.
     pickle.dump(lr_tfidf, open("saved_model.sav", 'wb'))
-    #  #  TODO - end of comment out the following for non gridsearch
-
-
-
+    #  TODO - end of comment out the following for non gridsearch
